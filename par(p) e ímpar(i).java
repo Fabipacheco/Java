dcfebfeac7828c98aebc52303b09aa8919101b5b
Fabipacheco <90794824+Fabipacheco@users.noreply.github.com>
2021-10-26 17:01:27 -0300

    Create par(p) e ímpar(i).java

---
+++ par(p) e ímpar(i).java
@@ -0,0 +1,16 @@
+public static void main(String[] args) {
+ int numero, p, i ;
+             Scanner teclado = new Scanner(System.in);
+
+                  System.out.println("Informe um número:");
+
+If ( numero % 2 == 0){
+p = numero;
+
+}else{
+i = numero
+
+System.out.println = (p)
+System.out.println = (i)
+
+teclado.close=();
